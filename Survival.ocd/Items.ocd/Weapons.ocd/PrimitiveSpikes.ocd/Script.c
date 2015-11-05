84a2d9d30441826269442df3f19a50473718cc8a
David Dormagen <czapper@gmx.de>
2015-11-05 19:46:57 +0100

    added primitive spikes (trap)

---
+++ Survival.ocd/Items.ocd/Weapons.ocd/PrimitiveSpikes.ocd/Script.c
@@ -0,0 +1,42 @@
+#include Library_Stackable
+
+public func MaxStackCount() { return 5; }
+public func TrapStrength() { return 15; }
+
+public func RejectUse(object clonk)
+{
+	return !clonk->IsWalking();
+}
+
+public func ControlUse(object clonk, int angle)
+{
+	clonk->DoKneel(true);
+	
+	var item = this->TakeObject();
+	
+	item->SetPosition(clonk->GetX(),clonk->GetY() + 7);
+	item->SetController(clonk->GetController());
+	item->AddTimer("HitCheck", 5);
+	item->SetR(-45);
+	item.Collectible = 0;
+	
+	Sound("GeneralHit?", nil, 20, nil, nil, nil, -100);
+}
+
+public func HitCheck()
+{
+	for (var obj in FindObjects(Find_AtRect(-5, -5, 10, 10), Find_Or(Find_OCF(OCF_Alive), Find_Func("IsProjectileTarget")), Find_Layer(), Find_Not(Find_Owner(GetController()))))
+	{
+		DealDamage(obj, TrapStrength(), DMG_Pierce);
+		if (obj)
+			obj->~OnProjectileHit(this);
+		SoundAt("OrganicHit*");
+		RemoveObject();
+		return;
+	}
+}
+
+local Collectible = 1;
+local Name = "$Name$";
+local Description = "$Description$";
+local Rebuy = true;
--- Survival.ocf/Test.ocs/Script.c
+++ Survival.ocf/Test.ocs/Script.c
@@ -4,6 +4,7 @@ func Initialize()
 	var chest = CreateObjectAbove(Chest, 50, 200, -1);
 	chest->CreateContents(Flintstones, 5);
 	chest->CreateContents(PrimitiveDarts, 5);
+	chest->CreateContents(PrimitiveSpikes, 5);
 	
 	CreateObjectAbove(StrawMan, 80, 200, -1);
 	CreateObjectAbove(StrawMan, 110, 200, -1);
