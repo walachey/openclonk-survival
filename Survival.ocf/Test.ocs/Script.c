70b20619f86ef42a2c5a4ad303c18bcc9506f9e6
David Dormagen <czapper@gmx.de>
2015-11-05 19:12:46 +0100

    test scenario: added chest

--- Survival.ocf/Test.ocs/Script.c
+++ Survival.ocf/Test.ocs/Script.c
@@ -1,11 +1,17 @@
 
+func Initialize()
+{
+	var chest = CreateObjectAbove(Chest, 50, 200, -1);
+	chest->CreateContents(Flintstones, 5);
+	chest->CreateContents(PrimitiveDarts, 5);
+	
+	CreateObjectAbove(StrawMan, 80, 200, -1);
+	CreateObjectAbove(StrawMan, 110, 200, -1);
+	CreateObjectAbove(StrawMan, 130, 200, -1);
+}
+
 func InitializePlayer(int plr)
 {
-	GetCrew(plr)->SetPosition(20,100);
-	GetCrew(plr)->CreateContents(Flintstones);
-	
-	CreateObjectAbove(StrawMan, 60, 200, -1);
-	CreateObjectAbove(StrawMan, 20, 200, -1);
-	CreateObjectAbove(StrawMan, 80, 200, -1);
+	GetCrew(plr)->SetPosition(20,100);	
 }
 
